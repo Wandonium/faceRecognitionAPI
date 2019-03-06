d4744591ce639a94586db292f58de8d973fcd3c9
Wandonium <hillarywando@gmail.com>
2019-03-06 10:56:28 +0300

    added validation and security features

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+// initialize Clarifai with api key.
+const app = new Clarifai.App({
+ apiKey: '1d8041663c54435aae1284d5d075b11d'
+});
+
+const handleApiCall = (req, res) => {
+	app.models.predict(
+		Clarifai.FACE_DETECT_MODEL, 
+      	req.body.input)
+	.then(data => res.json(data))
+	.catch(err => res.status(400).json('unable to work with API...'));
+}
 
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
@@ -11,5 +25,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-	handleImage
+	handleImage,
+	handleApiCall
 }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
@@ -32,6 +33,8 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
 
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+
+app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) });
 
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
