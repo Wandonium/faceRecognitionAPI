85e3fdf7785b3a946003de55c85c81a627349ec0
Wandonium <hillarywando@gmail.com>
2019-10-10 12:39:03 +0300

    change db host to heroku's route

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: ' postgresql-cylindrical-07189',
     user: 'postgres',
     password: 'wando214',
     database: 'smart-brain'
