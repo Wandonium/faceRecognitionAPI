04aa1628977163b5d4420c292b5e1636cbd74c9f
Wandonium <hillarywando@gmail.com>
2019-10-10 11:28:20 +0300

    working dev commit

--- controllers/image.js
+++ controllers/image.js
@@ -9,7 +9,9 @@ const handleApiCall = (req, res) => {
 	app.models.predict(
 		Clarifai.FACE_DETECT_MODEL, 
       	req.body.input)
-	.then(data => res.json(data))
+	.then(data => {
+		res.json(data)
+	})
 	.catch(err => res.status(400).json('unable to work with API...'));
 }
 
--- server.js
+++ server.js
@@ -10,11 +10,20 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({
+/*const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
    	ssl: true
+  }
+});*/
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'wando214',
+    database: 'smart-brain'
   }
 });
 
